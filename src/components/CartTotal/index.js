04dec8e640dd06d12f5895560eca6b6d47020cdc
kishore <ks.kishore59@gmail.com>
2021-10-10 15:34:21 +0000

    solution

---
+++ src/components/CartTotal/index.js
@@ -0,0 +1,38 @@
+import {Link} from 'react-router-dom'
+
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const CartTotal = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      let totalOrderCost = 0
+      cartList.forEach(eachCartItem => {
+        totalOrderCost += eachCartItem.cost * eachCartItem.quantity
+      })
+
+      return (
+        <>
+          <hr />
+          <div className="cart-summary-container">
+            <h1 className="order-total-value">Order Total:</h1>
+            <div className="total-container">
+              <p testid="total-price" className="total">
+                {totalOrderCost}
+              </p>
+              <Link to="/payment">
+                <button type="button" className="order-button">
+                  Place Order
+                </button>
+              </Link>
+            </div>
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default CartTotal
